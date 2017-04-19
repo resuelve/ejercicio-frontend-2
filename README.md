4dcb16af7dff6ead941f19daec30a05f2ca8c990
Julio Montaño <jmontano@resuelve.mx>
2017-04-19 13:03:12 -0500

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+Ejercicio para aplicar como front-end a Resuelve
+
+Estamos buscando experiencia con
+  - React
+    - Experiencia con React Router
+    - Experiencia con Moment
+    - Buen uso de `setState`
+    - Buen uso de `props`
+    - Uso de cosas como Makefiles, Fetch, Webpack, Cross-Env y demás son apreciados
+  - SCSS
+
+Cualquier otra experiencia es bienvenida.
+
+Clonar el repo, instalar dependencias y abrirlo en el navegador, el resto de instrucciones están ahí.
+
---
+++ src/employees/index.js
@@ -0,0 +1,94 @@
+let employees = [
+  {
+    id: 0,
+    name: 'Eduardo González',
+    company: 'América Móvil',
+    salary: 5400,
+    age: 57,
+    phone: '(886) 271 4211',
+    email: 'eduardo@gmail.com'
+  },
+  {
+    id: 1,
+    name: 'Miguel Ángel Rodríguez',
+    company: 'FEMSA',
+    salary: 11000,
+    age: 40,
+    phone: '(197) 651 8811',
+    email: 'miguel@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Mariana Gómez',
+    company: 'Banorte',
+    salary: 16900,
+    age: 34,
+    phone: '(396) 702 3554',
+    email: 'mariana@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Alejandro Fernández',
+    company: 'Grupo México',
+    salary: 17700,
+    age: 41,
+    phone: '(542) 591 3978',
+    email: 'alejandro@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Gabriela López',
+    company: 'Grupo Inbursa',
+    salary: 10100,
+    age: 65,
+    phone: '(783) 560 4847',
+    email: 'gabriela@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Alejandra Díaz',
+    company: 'Cemex',
+    salary: 6500,
+    age: 27,
+    phone: '(862) 721 8605',
+    email: 'alejandra@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Juan Carlos Martínez',
+    company: 'Grupo Televisa',
+    salary: 14000,
+    age: 38,
+    phone: '(973) 322 3993',
+    email: 'juan@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'José Luis Pérez',
+    company: 'Alfa',
+    salary: 20400,
+    age: 50,
+    phone: '(226) 806 3427',
+    email: 'jose@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'María Carmen García',
+    company: 'Grupo Bimbo',
+    salary: 23700,
+    age: 29,
+    phone: '(907) 702 7639',
+    email: 'maria@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Adriana Sánchez',
+    company: 'Liverpool',
+    salary: 33800,
+    age: 23,
+    phone: '(248) 255 7702',
+    email: 'adriana@gmail.com'
+  }
+]
+
+export default employees
