248f17d3e54ae7e0197e543ecdc5c13a78d9658c
Diego Viniegra <diego.viniegra@gmail.com>
2018-08-01 09:22:10 -0500

    doc: Se documenta main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+/**
+ * Entrypoint.
+ *
+ * Se incluyen los componentes globales, css, filtros y se levanta la
+ * instancia de Vue
+ */
+
 import Vue from 'vue'
 import App from './App.vue'
 
+// Se importan los componentes globales
 import Instructions from './components/Instructions'
-
+// Se importan los filtros para dar formato a los datos (ya activos)
 import './filters'
 
+// Se registran los componentes globales
 Vue.component('Instructions', Instructions)
 
 new Vue({
