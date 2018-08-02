987de95791864d60a489eac58ec863e9b984e332
Diego Viniegra <diego.viniegra@gmail.com>
2018-08-02 01:21:26 -0500

    fea: Se agregan patterns al formulario
    Se agregan patterns con RegExp en los campos del formulario de
    AddEmployee para forzar el cumplimiento.

--- src/components/Employees/AddEmployee.vue
+++ src/components/Employees/AddEmployee.vue
@@ -3,30 +3,63 @@
     <div class="">
       <h4>Agregar Empleado</h4>
     </div>
-    <form class="" @submit.prevent="store" method="post">
+    <form @submit.prevent="store" method="post">
         <div class="mdl-textfield mdl-js-textfield">
-          <input id="form_name" class="mdl-textfield__input" type="text" v-model="create.name" placeholder="Nombre" />
+          <!-- Campo Nombre -->
+          <input
+            class="mdl-textfield__input"
+            type="text"
+            v-model="create.name"
+            pattern="[A-Z](\.|[a-z]+)( [A-Z](\.|[a-z]+))( [A-Z](\.|[a-z]+))?" required
+            placeholder="Nombre" />
           <span class="mdl-textfield__error">error</span>
         </div>
         <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textfield__input" type="text" v-model="create.company" placeholder="Compañía" />
-        </div>
-
-        <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textfield__input" type="text" v-model="create.age" placeholder="Edad" />
-        </div>
-        <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textfield__input" type="text" v-model="create.salary" placeholder="Salario" />
+          <!-- Campo Empresa -->
+          <input
+            class="mdl-textfield__input"
+            type="text"
+            v-model="create.company"
+            placeholder="Compañía" />
         </div>
         <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textfield__input" type="text" v-model="create.phone" placeholder="Teléfono" />
-
+          <!-- Campo Edad -->
+          <input
+            class="mdl-textfield__input"
+            type="text"
+            v-model="create.age"
+            pattern="\d{2}|11\d"
+            placeholder="Edad" />
         </div>
         <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textfield__input" type="text" v-model="create.email" placeholder="Correo" />
-
+          <!-- Campo Salario -->
+          <input
+            class="mdl-textfield__input"
+            type="text"
+            v-model="create.salary"
+            pattern="\d*(\.\d+)?" required
+            placeholder="Salario" />
+        </div>
+        <!-- Campo Teléfono -->
+        <div class="mdl-textfield mdl-js-textfield">
+          <input
+            class="mdl-textfield__input"
+            type="text"
+            v-model="create.phone"
+            pattern="^\(\d{3}\) \d{3} \d{4}$"
+            placeholder="Teléfono" />
+        </div>
+        <div class="mdl-textfield mdl-js-textfield">
+          <!-- Campo Correo -->
+          <input
+            class="mdl-textfield__input"
+            type="text"
+            v-model="create.email"
+            pattern=".+@[a-z].*\..*"
+            placeholder="Correo" />
         </div>
         <div class="">
+          <!-- Se utiliza esta forma de submit para utilizar los patterns -->
           <button class="mdl-button" type="submit" name="button">Guardar</button>
           <button class="mdl-button" @click.prevent="$emit('cancel')">Cancelar</button>
         </div>
@@ -72,7 +105,7 @@ export default {
       this.create.id = this.employees.reduce((acc, item) => {
         return item.id > acc.id ? item : acc
       }).id + 1
-      
+
       // Se crea un clon de los datos y se puja
       this.employees.push(JSON.parse(JSON.stringify(this.create)))
       this.$emit('saved')
